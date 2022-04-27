216553528532de109a7d7f5a24a0eb7cde558ff5
santoshmohan04 <santosh_tagore04@rediffmail.com>
2022-04-27 23:24:10 +0530

    angular based student app using firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "xxxxxx-xxxxxx_xxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxx_xxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxx",
-    appId: "1:xxxxxxxxxxxxxxxxxx:web:xxxxxxxxxxxxxxxxxx"
+    apiKey: "AIzaSyCwXfpaHvKX_saY8CQUw-hVHeiguFEtKSU",
+    authDomain: "pick-and-pack-102af.firebaseapp.com",
+    databaseURL: "https://pick-and-pack-102af.firebaseio.com",
+    projectId: "pick-and-pack-102af",
+    storageBucket: "pick-and-pack-102af.appspot.com",
+    messagingSenderId: "162417798881",
+    appId: "1:162417798881:web:bc8a6ee690b9d2edeb51a0",
+    measurementId: "G-J3G9BYZ205"
   }
 };
